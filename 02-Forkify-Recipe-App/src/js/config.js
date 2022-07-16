e4de7787b646a4828a29e0de2cf46eb79780c935
Timothy Zammit <96834656+timmaz47@users.noreply.github.com>
2022-07-16 11:46:36 +0200

    Initial commit

---
+++ 02-Forkify-Recipe-App/src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '434a2ce2-5c3d-4f0b-8c2d-a88351cc8aef';
+export const MODAL_CLOSE_SEC = 2.5;
